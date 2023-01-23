18eaefcca01859501b61a0d6b9a2f089e5b348b9
mosaber4444 <myemail>
2023-01-23 19:28:22 +0330

    mmd change

--- solution/js/main.js
+++ solution/js/main.js
@@ -3,6 +3,7 @@ let select = $('#select');
 let NativeName = $('#NativeName');
 let Capital = $('#Capital');
 let Region = $('#Region');
+let callingCodes = $('#callingCodes');
 let Population = $('#Population');
 let Languages = $('#Languages');
 let TimeZone = $('#TimeZone');
@@ -20,10 +21,14 @@ function showResult() {
     Capital.text(array[indexSelect]["capital"]);
     Region.text(array[indexSelect]["region"]);
     Population.text(array[indexSelect]["population"]);
+    callingCodes.text(array[indexSelect]["callingCodes"]);
     Languages.text(Object.values(array[indexSelect]["languages"])[0]);
     TimeZone.text(Object.values(array[indexSelect]["timezones"])[0]);
     flag.attr('src', (array[indexSelect]["flags"]));
-    maps.attr('src', (array[indexSelect]["maps"]));
+    maps.attr('src',`https://maps.google.com/maps?width=100%&height=500&hl=en&q=${array[indexSelect]["name"]} (Title)&ie=UTF8&t=&z=10&iwloc=B&output=embed`);
 }
 
 showResult();
+
+
+
